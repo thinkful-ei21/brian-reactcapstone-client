504c343a209dea5844a9a30a4598fa9ffb26ccf1
Brian Dixon <briandixon@Brians-Mac-mini.home>
2018-07-13 16:45:08 -0400

    onboard

--- src/components/onBoard.js
+++ src/components/onBoard.js
@@ -6,14 +6,18 @@ export default class Onboard extends React.Component{
         return(
         <div className="onboarding">
             <h1 className="lyricbanner">LYRIC CREATOR</h1>
-            <h2>What do I do?</h2>
+            <h2>What does this app do?</h2>
              <p>This is a Lyric creator app.</p>
          <ol>
             <li>Maybe you are curious about making a song and need a small place to jot down some ideas quickly. </li>
-            <li>You need to <strong>jot down</strong> some notes inbetween a recording session.</li>
-            <li>You want to make quick <strong>comments</strong> on how close your lyrics are to perfection.</li>
+            <li>You need to <strong>jot down</strong> some notes inbetween a recording session for the world to see.</li>
+            <li>You want to make quick <strong>comments</strong> on how close your lyrics are to perfection, or someone elses song.</li>
          </ol>
-            <p> a personal Genius app for yourself and friends. </p>
+            <h2> A personal Genius app for yourself and friends.  </h2>
+            <h2> This application was designed in order to help anyone jot down lyrics for a song and share them with the world, and open them for commentary. Normally it is difficult to send
+                 songs to Genius and allow them to feature your lyrics for the world to see. But now anyone is capable of doing this. </h2>
+            
+                 
         </div>
         )
     }
